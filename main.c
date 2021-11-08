18447a6edb2adc47745c919c48793df1e5160ed8
DavidOP1 <54214707+DavidOP1@users.noreply.github.com>
2021-11-08 18:06:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
     if(num2<num1) {temp=num2; num2=num1; num1=temp;}
     for(int i=num1;i<=num2;i++){
        if(i==num1) printf("The Armstrong numbers are:");
-       if(isArmstrong(i)) printf(" %d",i);
+       if(isArmstrong(i)) printf("%d",i);
        if(i==num2) printf("\n");
     } 
      for(int i=num1;i<=num2;i++){
